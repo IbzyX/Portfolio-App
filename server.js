31e4f4da901921b93c0194bd050802c6782443f5
ibrahim <ibrahimrahman2806@gmail.com>
2025-10-03 03:52:19 +0100

    Initial commit

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'static')));
+
 app.get('/', (req, res) => {
-    res.send('Hello from Node.js and Express!');
+    res.sendFile(path.join(__dirname, 'static', 'index.html'));
 });
 
 app.listen(port, () => {
